e9f5555dde2bca0d792a4f6dc3bb31ca52c7fdd9
Fabian Jahr <fabian.jahr@whu.edu>
2016-11-19 14:49:25 +0100

    call out to ebay api

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var express = require('express')
+var http = require('http')
+var querystring = require('querystring');
 var app = express()
+var url = "preisfinder.ebay-kleinanzeigen.de"
+
+var options = function(query) {
+  path = "/datafeed?cmd=searcheBayClassifieds&keyword=" + query
+
+  return { host: url,
+    path: path,
+    port: 80,
+    agent: false
+  }
+};
+// curl "http://preisfinder.ebay-kleinanzeigen.de/datafeed?cmd=searcheBayClassifieds&keyword=iphone+4&item_display_size=8&mobile=0" --basic -u ebayk_hackathon_chatbot:cf73e71aa2721eba
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -9,6 +23,16 @@ app.get('/', function(request, response) {
 })
 
 app.get('/api', function(request, response) {
+  query = querystring.escape(request.query.q)
+  opt = options(query)
+
+  http.get(opt, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+
   response.json({message: 'Hello World!'})
 })
 
